fe97c9d6b34123a11aad76bc3face352ed295ed7
george abou assaleh <gja07@mail.aub.edu>
2025-11-09 13:49:32 +0200

    fixed test file

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,8 +1,10 @@
 import unittest
 from app import greet
+
 class TestApp(unittest.TestCase):
+
     def test_greet(self):
-        self.assertEqual(greet("World"), "Hello, World from FirstName LastName!")
+        self.assertEqual(greet("World from George Abou Assaleh"), "Hello, World from George Abou Assaleh!")
 
 if __name__ == "__main__":
     unittest.main()
