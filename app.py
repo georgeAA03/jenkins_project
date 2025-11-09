8af7200d4a5b78da0fc41016d157e4081f237376
george abou assaleh <gja07@mail.aub.edu>
2025-11-09 15:04:59 +0200

    Fixed app.py indentations

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
-# app.py
 from flask import Flask
+
 
 app = Flask(__name__)
 
+
 @app.route('/<name>')
 def greet(name):
-
     return f"Hello, {name}!"
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)  # runs on localhost:5000
